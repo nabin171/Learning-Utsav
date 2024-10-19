da0f6a11186b880e7a9f78d7b8964ebe600f387e
Nabin Karki <karki0008@gmail.com>
2024-10-19 12:32:04 +0545

    facebook reaction practice

--- React _Home/client/src/app/dummy/page.js
+++ React _Home/client/src/app/dummy/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { FaAngry } from "react-icons/fa";
 import { IoIosHappy } from "react-icons/io";
@@ -8,28 +8,33 @@ import { FaHeart } from "react-icons/fa";
 import { AiFillLike } from "react-icons/ai";
 import { Button } from "@nextui-org/react";
 
-
 const Reactions = () => {
-let [isReactionDivUpon,setIsReactionDivUpon]=useState(false);
-let [isLiked, setIsLiked] = useState(false);
+  let [isReactionDivOpen, setIsReactionDivOpen] = useState(false);
+  let [isLiked, setIsLiked] = useState(false);
+  let [isSmall,SetIsSmall]=useState(false);
   return (
     <div>
-      {isReactionDivUpon && (
-        <div className="bg-black p-2 m-2 shadow-lg border border-gray flex w-[20%] rounded-md">
+      {isReactionDivOpen && (
+        <div className="bg-black p-2 m-2 shadow-lg border border-gray flex w-[15%] rounded-md">
           <AiFillLike
-            size={40}
-            color="blue"
-    
+            size={isSmall ? "40" : "80"}
+            color={isLiked ? "blue" : "red"}
+            onMouseOver={() => SetIsSmall(true)}
           />
           <FaSadCry size={40} color="#ffcc33" />
           <FaAngry size={40} color="#f64d52" />
           <IoHappy size={40} color="#ffcc4d" />
-          <FaHeart size={40} color="U+1F49B" />
+          <FaHeart size={40} color="red" />
         </div>
       )}
-      <Button className=""onMouseEnter={() => setIsReactionDivUpon(true)} onClick={()=>setIsLiked(true)}>
-        <AiFillLike size={80} color={isLiked?"blue":"red"}/>
-        Like
+
+      <Button
+        onMouseEnter={() => setIsReactionDivOpen(true)}
+        onClick={() => setIsLiked(true)}
+        className=""
+      >
+        {" "}
+        <AiFillLike size={40} color={isLiked ? "blue" : "red"} /> Like
       </Button>
     </div>
   );
